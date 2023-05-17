fd351e2858d8654efe7a01d55beeda307300cb72
lucifersahil0001 <harshgupta00010@gmail.com>
2023-05-17 11:22:02 +0530

    navbar assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./components/Home"
+import About from "./components/About"
+import Signup from './components/Signup';
+import Navbar from "./components/Navbar/Navbar";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Link to={'/home'}> home</Link>
+        <Link to={'/login'}>go to login</Link>
+        <Link to={'/Register'}>go to Register </Link> */}
+        <BrowserRouter>
+        <Navbar/> 
+        <Routes>
+          <Route path='/' element = { <Home/>}/>
+          <Route path='/about' element = { <About/>}/>
+          <Route path='/signup' element = { <Signup/>}/>
+          
+          
+
+
+         
+        </Routes>
+
+
+        </BrowserRouter>
+        
+      
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import { useState } from 'react'
+
+
+
+
+const Navbar = () => {
+  const [isMobile,setIsMobile] = useState(false)
+
+ return (
+    <>
+     
+
+
+
+      <div className='navbar'>
+        <h3 className='logo'>ShopVibe</h3>
+ 
+        <div className= {isMobile? "nav-links-mobile" : "nav-links" }
+        onClick={()=> setIsMobile(false)}
+
+
+        >
+
+        
+        <Link to='/' className='home'> Home</Link>
+        <Link to={'/about'} className='about'>About</Link>
+        <Link to={'/login'} className='login'>Login</Link>
+        </div>
+
+        {/* <button className='mobile-menu-icon'
+        onClick={()=>setIsMobile(!isMobile)}
+        
+        >
+           {  isMobile ?    <CloseOutlinedIcon/>:  <MenuOutlinedIcon />}
+            
+        
+           
+           
+        </button> */}
+
+      </div>
+       
+    </>
+ )
+}
+
+export default Navbar
